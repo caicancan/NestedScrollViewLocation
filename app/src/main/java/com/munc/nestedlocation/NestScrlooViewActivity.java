e00cd1fa2fe786d50da341c3d65fed07575420d2
caicancan <18062411472@163.com>
2018-05-24 10:24:44 +0800

    添加NestScrollview中对于高度滑动的计算和设定

--- app/src/main/java/com/munc/nestedlocation/NestScrlooViewActivity.java
+++ app/src/main/java/com/munc/nestedlocation/NestScrlooViewActivity.java
@@ -1,5 +1,6 @@
 package com.munc.nestedlocation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,39 +13,49 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NestScrlooViewActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    private TabLayout tab;
-    private NestedScrollView nestedScrollView;
+public class NestScrlooViewActivity extends AppCompatActivity {
+    @BindView(R.id.tab)
+    TabLayout tab;
+    @BindView(R.id.mnestedScrollView)
+    NestedScrollView nestedScrollView;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nest_scrloo_view);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         initData(1);
         initView();
     }
+
+    @OnClick(R.id.fab)
+    public void onViewClicked() {
+        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+
     /**
      * 刷新ScrollView的内容
      */
+    @BindView(R.id.ll_sc_content)
+    LinearLayout layout;
     private void setScrollViewContent() {
         //NestedScrollView下的LinearLayout
-        LinearLayout layout = (LinearLayout) findViewById(R.id.ll_sc_content);
         layout.removeAllViews();
         for (int i = 0; i < mData.size(); i++) {
             View view = View.inflate(this, R.layout.item_layout, null);
@@ -54,10 +65,10 @@ public class NestScrlooViewActivity extends AppCompatActivity {
         }
     }
     private String[] titles={"1","2","3","4","5","6","7","8","9","10"};
+
+
     private void initView() {
         //添加tab
-        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
-        tab = (TabLayout) findViewById(R.id.tab);
         tab.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorAccent));
         tab.setTabTextColors(getResources().getColor(R.color.black),getResources().getColor(R.color.red));
         for (int i = 0; i <titles.length ; i++) {
@@ -66,8 +77,7 @@ public class NestScrlooViewActivity extends AppCompatActivity {
         tab.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                initData(tab.getPosition() + 1);
-                setScrollViewContent();
+
             }
 
             @Override
@@ -81,13 +91,46 @@ public class NestScrlooViewActivity extends AppCompatActivity {
             }
         });
         setScrollViewContent();
-//        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-//            @Override
-//            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-//
-//            }
-//        });
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                Toast.makeText(getApplicationContext(), "" + scrollY, Toast.LENGTH_SHORT).show();
+                if (scrollY>0&&scrollY<getChildHeight(1)){
+                    tab.setScrollPosition(0,0,true);
+                }else if (scrollY>getChildHeight(1)&&scrollY<getChildHeight(2)){
+                    tab.setScrollPosition(0,1,true);
+                }else if (scrollY>getChildHeight(2)&&scrollY<getChildHeight(3)){
+                    tab.setScrollPosition(0,2,true);
+                }else if (scrollY>getChildHeight(3)&&scrollY<getChildHeight(4)){
+                    tab.setScrollPosition(0,3,true);
+                }else if (scrollY>getChildHeight(4)&&scrollY<getChildHeight(5)){
+                    tab.setScrollPosition(0,4,true);
+                }else if (scrollY>getChildHeight(5)&&scrollY<getChildHeight(6)){
+                    tab.setScrollPosition(0,5,true);
+                }else if (scrollY>getChildHeight(6)&&scrollY<getChildHeight(7)){
+                    tab.setScrollPosition(0,6,true);
+                }else if (scrollY>getChildHeight(7)&&scrollY<getChildHeight(8)){
+                    tab.setScrollPosition(0,7,true);
+                }else if (scrollY>getChildHeight(8)&&scrollY<getChildHeight(9)){
+                    tab.setScrollPosition(0,8,true);
+                }else if (scrollY>getChildHeight(9)&&scrollY<getChildHeight(10)){
+                    tab.setScrollPosition(0,9,true);
+                }
 
+            }
+        });
+
+    }
+    //计算向下滑动的距离
+    int count;
+    int height;
+    private int getChildHeight(int i) {
+        count=0;height=0;
+        for (int a = 0; a < i; a++) {
+            height = layout.getChildAt(a).getHeight();
+            count = height + count;
+        }
+        return count;
     }
 
     //加载右边的menu布局
